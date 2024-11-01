88c6a89b37da2ac01464ed5e0cd37c21929571c2
David Vilar <david.vilar.gallego@estudiantat.upc.edu>
2024-11-01 16:54:04 +0100

    Actualizacion player listener

--- src/main/java/dc/listeners/PlayerListener.java
+++ src/main/java/dc/listeners/PlayerListener.java
@@ -1,9 +1,13 @@
 package dc.listeners;
 
 import dc.DeathScape;
+import dc.config.PlayerData;
+import dc.config.PlayerDatabase;
+import dc.config.PlayerEditDatabase;
 import dc.player.PlayerDeath;
 import dc.server.ServerData;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -52,17 +56,48 @@ public class PlayerListener implements Listener {
     }
 
     @EventHandler
-    public void onChat(AsyncPlayerChatEvent event) {
-        // Futura implementación para prohibir palabras.
-    }
-
-    @EventHandler
     public void onPlayerJoinServer(PlayerJoinEvent event) {
-        // Código de gestión de conexión y datos del jugador.
+        Player player = event.getPlayer();
+        String hostaddress = Objects.requireNonNull(player.getAddress()).getAddress().getHostAddress();
+
+        if (PlayerDatabase.getPlayerDataFromDatabase(player.getName()) == null) {
+            PlayerData playerData = new PlayerData(player.getName(), false, 0, hostaddress, "0", player.getUniqueId(), "0", "0", "0");
+            if (!PlayerDatabase.addPlayerDataToDatabase(playerData)) {
+                player.kickPlayer(ChatColor.RED + "Error al cargar tus datos, contacta con un administrador.");
+            }
+        } else {
+            PlayerData playerData = PlayerDatabase.getPlayerDataFromDatabase(player.getName());
+            assert playerData != null;
+            if (playerData.isDead() && !player.isOp()) {
+                player.kickPlayer(ChatColor.RED + "Estás muerto, contacta con un administrador.");
+            }
+            if (!Objects.equals(playerData.getHostAddress(), hostaddress) && plugin.getMainConfigManager().isKick_if_ip_changed() && !player.isOp()) {
+                player.kickPlayer(ChatColor.RED + "Tu IP ha cambiado, contacta con un administrador.");
+            }
+        }
+
+        if (player.isOnline()) {
+            plugin.tiempoDeConexion.put(player.getName(), System.currentTimeMillis());
+        }
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        // Código para calcular el tiempo de conexión del jugador.
+        Player player = event.getPlayer();
+        String nombreJugador = player.getName();
+
+        if (plugin.tiempoDeConexion.containsKey(nombreJugador)) {
+            long tiempoDeInicio = plugin.tiempoDeConexion.get(nombreJugador);
+            long tiempoActual = System.currentTimeMillis();
+            long tiempoEnLinea = tiempoActual - tiempoDeInicio;
+
+            int segundos = Integer.parseInt(String.valueOf((int) (tiempoEnLinea / 1000) % 60));
+            int minutos = Integer.parseInt(String.valueOf((int) ((tiempoEnLinea / (1000 * 60)) % 60)));
+            int horas = Integer.parseInt(String.valueOf((int) ((tiempoEnLinea / (1000 * 60 * 60)) % 24)));
+            PlayerEditDatabase.setPlayerTimePlayed(player, segundos, minutos, horas);
+            plugin.tiempoDeConexion.remove(nombreJugador);
+        }
+
+        PlayerEditDatabase.setPlayerCoords(player);
     }
 }
